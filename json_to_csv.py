2be303455ab9e951eb2b3147e6da312b78d04229
rushi <rushi@rushis-MacBook-Pro.local>
2024-09-17 13:53:37 +1000

    json to csv

---
+++ json_to_csv.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+
+data = []
+with open("../output_json", "r") as f:
+    for line in f:
+        data.append(json.loads(line))
+
+
+df = pd.DataFrame(data= data)
+df.to_csv("../final_flows.csv")
