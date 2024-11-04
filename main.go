c4aa8121db7d4da11acbefdd458bd3777e1bfff6
rushi <rushi@rushis-MacBook-Pro.local>
2024-11-04 11:52:22 +1100

    logging fix

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	//mn_timestamp, _ := time.Parse("2006-01-02 15:04:05.000000 -0700 MST", "2024-09-18 08:02:52.138703 +0000 UTC")
 	//mx_timestamp, _ := time.Parse("2006-01-02 15:04:05.000000 -0700 MST", "2024-09-20 06:52:26.436679 +0000 UTC")
-	fmt.Println(mn_timestamp, mx_timestamp)
+	fmt.Println("min max timestamps = ", mn_timestamp, mx_timestamp)
 	flow_map := core.GetFlowTupleToFlowInfo(ground_truth_csvs, mn_timestamp, mx_timestamp)
 	fmt.Println(len(flow_map))
 	core.MatchPcaps(pacp_file_path, output_path, flow_map)
